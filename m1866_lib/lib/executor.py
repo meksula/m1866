2f6a4b727a76161e6ab669e327caf358049d19db
Karol Meksuła <meksula.karol.93@gmail.com>
2019-10-12 22:36:45 +0200

    Shoot executor changes - url builder with params implemented

--- m1866_lib/lib/executor.py
+++ m1866_lib/lib/executor.py
@@ -1,39 +1,93 @@
 import requests
 from colorama import Fore, Style
 
+from m1866_lib.lib.history import History
 from m1866_lib.lib.objects import Smoke
+
 
 http_methods = {
     "get": lambda shoot: http_get(shoot),
-    "post": lambda shoot: http_post(shoot)
+    "post": lambda shoot: http_post(shoot),
+    "put": lambda shoot: http_put(shoot),
+    "patch": lambda shoot: http_patch(shoot),
+    "delete": lambda shoot: http_delete(shoot)
 }
 
 
 def execute_request(shoot):
-    shoot = pre_valid(shoot)
     if shoot is not None:
         resp = http_methods[shoot["http_method"]](shoot)
-        return Smoke(shoot, resp.content, resp.cookies, resp.headers, resp.status_code, resp.url)
+        smoke = Smoke(shoot, resp.content, resp.cookies, resp.headers, resp.status_code, resp.url)
+        if 'store_history' in shoot.keys() and shoot['store_history'] is True:
+            History().save(smoke)
+            return smoke
+        return smoke
     else:
         print(Fore.RED, 'Your request is invalid', Style.RESET_ALL)
 
 
+# Shoot input object must be validated before execute request
+# You will receive information about validation errors etc.
 def pre_valid(shoot):
-    # TODO
     return shoot
 
 
 def http_get(shoot):
-    # TODO
+    shoot = pre_valid(shoot)
     url = build_url(shoot)
     return requests.get(url)
 
 
 def http_post(shoot):
+    shoot = pre_valid(shoot)
+    pass
+
+
+def http_put(shoot):
+    shoot = pre_valid(shoot)
+    pass
+
+
+def http_patch(shoot):
+    shoot = pre_valid(shoot)
+    pass
+
+
+def http_delete(shoot):
+    shoot = pre_valid(shoot)
     pass
 
 
 def build_url(shoot):
-    # TODO
     url = shoot["url"]
-    return url["protocol"] + "://" + url["domain"] + url["uri"]
+    keys = url.keys()
+
+    def compose(domain):
+        # Default placeholder syntax:
+        # ${param} - this will be replaced with provided parameters in list shoot.url.params
+        # Param must have two fields: `key` and `value`
+        def params_attach(complete_url):
+            try:
+                param_list = shoot["url"]["params"]
+                for param in param_list:
+                    old = '${' + param["key"] + '}'
+                    new = param["value"]
+                    complete_url = complete_url.replace(old, new)
+                return complete_url
+            except KeyError:
+                return complete_url
+        protocol = 'http'
+        uri = '/'
+        if "protocol" in keys:
+            protocol = url["protocol"]
+        if "uri" in keys:
+            uri = url["uri"]
+        return params_attach(protocol + "://" + domain + uri)
+
+    if "domain" not in keys and "ip" not in keys:
+        print(Fore.RED, 'Both domain name and IPv4 address is not specified!', Style.RESET_ALL)
+    if "domain" in keys:
+        return compose(url["domain"])
+    else:
+        if "ip" in keys:
+            return compose(url["ip"])
--- tests/lib/test_executor.py
+++ tests/lib/test_executor.py
@@ -1,5 +1,9 @@
 import unittest
-from m1866_lib.lib.executor import execute_request
+from m1866_lib.lib.executor import execute_request, build_url
+
+min_url = "https://jsonplaceholder.typicode.com/todos/1"
+min_url_ip = "https://172.64.128.28/todos/1"
+base_url = "http://172.64.128.28/"
 
 
 class TestExecutor(unittest.TestCase):
@@ -15,7 +19,36 @@ class TestExecutor(unittest.TestCase):
         self.assertIsNotNone(smoke.timestamp)
         self.assertIsNotNone(smoke.response_status)
         self.assertEqual(smoke.response_status, 200)
-        self.assertEqual(smoke.url, "https://jsonplaceholder.typicode.com/todos/1")
+        self.assertEqual(smoke.url, min_url)
+
+    def test_build_url(self):
+        url = build_url(minimalistic_shoot())
+        print(url)
+        self.assertEqual(url, min_url)
+
+    def test_build_url_ip(self):
+        url = build_url(min_shoot_ip())
+        self.assertEqual(url, min_url_ip)
+
+    def test_build_url_both_domain_and_ip_are_not_specified(self):
+        shoot = no_domain_and_ip_shoot()
+        result_url = build_url(shoot)
+        self.assertEqual(result_url, None)
+
+    def test_build_url_with_only_domain_name(self):
+        shoot = only_ip_shoot()
+        result_url = build_url(shoot)
+        self.assertEqual(result_url, base_url)
+
+    def test_build_url_with_one_placeholder(self):
+        shoot = one_param_shoot()
+        result_url = build_url(shoot)
+        self.assertEqual(min_url, result_url)
+
+    def test_build_url_with_multi_placeholders(self):
+        shoot = multi_param_shoot()
+        result_url = build_url(shoot)
+        self.assertEqual(min_url, result_url)
 
 
 def minimalistic_shoot():
@@ -25,5 +58,73 @@ def minimalistic_shoot():
             "protocol": "https",
             "domain": "jsonplaceholder.typicode.com",
             "uri": "/todos/1"
+        }
+    }
+
+
+def min_shoot_ip():
+    return {
+        "http_method": "get",
+        "url": {
+            "protocol": "https",
+            "ip": "172.64.128.28",
+            "uri": "/todos/1"
+        }
+    }
+
+
+def no_domain_and_ip_shoot():
+    return {
+        "http_method": "get",
+        "url": {
+            "protocol": "https",
+            "uri": "/todos/1"
+        }
+    }
+
+
+def only_ip_shoot():
+    return {
+        "http_method": "get",
+        "url": {
+            "ip": "172.64.128.28",
+        }
+    }
+
+
+def one_param_shoot():
+    return {
+        "http_method": "get",
+        "url": {
+            "protocol": "https",
+            "domain": "jsonplaceholder.typicode.com",
+            "uri": "/${example_param}/1",
+            "params": [
+                {
+                    "key": "example_param",
+                    "value": "todos"
+                }
+            ]
+        }
+    }
+
+
+def multi_param_shoot():
+    return {
+        "http_method": "get",
+        "url": {
+            "protocol": "https",
+            "domain": "jsonplaceholder.typicode.com",
+            "uri": "/${example_param}/${example_param_2}",
+            "params": [
+                {
+                    "key": "example_param",
+                    "value": "todos"
+                },
+                {
+                    "key": "example_param_2",
+                    "value": "1"
+                }
+            ]
         }
     }
